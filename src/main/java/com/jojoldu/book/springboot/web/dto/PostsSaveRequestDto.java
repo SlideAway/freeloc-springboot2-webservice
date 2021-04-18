b679888e37d9c788745bdf34f1cb6e87e718c3ef
gkszm <gkszm636@gmail.com>
2021-04-18 23:25:42 +0900

    4장 완료

--- src/main/java/com/jojoldu/book/springboot/web/dto/PostsSaveRequestDto.java
+++ src/main/java/com/jojoldu/book/springboot/web/dto/PostsSaveRequestDto.java
@@ -8,15 +8,15 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class PostsSaveRequestDto {
-    private String title;
-    private String content;
-    private String author;
+    private String  title   ;
+    private String  content ;
+    private String  author  ;
 
     @Builder
     public PostsSaveRequestDto(String title, String content, String author) {
-        this.title = title;
-        this.content = content;
-        this.author = author;
+        this.title      = title     ;
+        this.content    = content   ;
+        this.author     = author    ;
     }
 
     public Posts toEntity() {
--- src/main/java/com/jojoldu/book/springboot/web/dto/PostsUpdateRequestDto.java
+++ src/main/java/com/jojoldu/book/springboot/web/dto/PostsUpdateRequestDto.java
@@ -7,13 +7,13 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class PostsUpdateRequestDto {
-    private String title;
-    private String content;
+    private String  title   ;
+    private String  content ;
 
     @Builder
     public PostsUpdateRequestDto(String title, String content) {
-        this.title = title;
-        this.content = content;
+        this.title      = title     ;
+        this.content    = content   ;
     }
 
 }
